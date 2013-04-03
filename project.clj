556551fd7596de7bc30dafcb99c487858034e29d
Bak Yeon O <bakyeono@gmail.com>
2013-04-04 07:19:08 +0900

    0.0.3 - 데이터 업데이트

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject l21p "0.0.2"
+(defproject l21p "0.0.3"
   :description ""
   :url "https://github.com/bakyeono/l21p"
-  :license {:name "GNU General Public License (version 3)"
-            :url "http://www.gnu.org/licenses/gpl.html"}
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [crypto-random "1.1.0"]]
   :java-source-paths ["src"]
